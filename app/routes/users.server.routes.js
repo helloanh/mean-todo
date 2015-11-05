7cf037db1b7aaec901d208718a7a7aafe5f9682f
skatermonk88 <akhoang88@gmail.com>
2015-11-05 13:14:09 +0700

    add routeing for users

---
+++ app/routes/users.server.routes.js
@@ -0,0 +1,8 @@
+var users = require('../../controllers/users.server.controller');
+
+// RESTful API best practice, call route users
+// sending HTTP post request to the base users route
+
+module.exports = function(app){
+    app.route('/users').post(users.create);
+}
