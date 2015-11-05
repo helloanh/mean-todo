6e3d776239990fa13e5165723fbc8da2a758661b
skatermonk88 <akhoang88@gmail.com>
2015-11-05 12:29:23 +0700

    bug js-bson: Failed to load c++ bson extension

--- config/env/development.js
+++ config/env/development.js
@@ -1,6 +1,6 @@
 module.exports = {
   // development configuration options
-    db:'mongodb://localhost/mean-book'    
+    db:'mongodb://localhost/mean-book'   , 
     sessionSecret: 'developmentSessionSecret-X79Vn234239dfjl*&2nH'
 };
 
