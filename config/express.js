c0e2c859995f282996635add191692bf9554971e
skatermonk88 <akhoang88@gmail.com>
2015-11-05 13:16:51 +0700

    update users routes

--- config/express.js
+++ config/express.js
@@ -34,6 +34,8 @@ module.exports = function() {
 
     require('../app/routes/index.server.routes.js')(app);
 
+    require('../app/routes/users.server.routes.js')(app);
+
     app.use(express.static('./public'));
 
     return app;
