6a32f5a8ff3aced343c374b8d6a11603e013e7b9
skatermonk88 <akhoang88@gmail.com>
2015-11-05 13:33:19 +0700

    add route for list() method

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,8 +1,12 @@
-var users = require('../../controllers/users.server.controller');
+var users = require('../../app/controllers/users.server.controller');
 
 // RESTful API best practice, call route users
 // sending HTTP post request to the base users route
 
 module.exports = function(app){
-    app.route('/users').post(users.create);
-}
+    app.route('/users')
+        .post(users.create)
+        .get(users.list);
+};
+
+
