1324f9c4a2d9f1ff8adac6614cbc9ab9b550c503
skatermonk88 <akhoang88@gmail.com>
2015-11-05 18:28:22 +0700

    add validators

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -2,16 +2,47 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    firstName:String,
-    lastName:String,
-    email:String,
-    username: String,
-    password: String,
+    firstName: String,
+    lastName: String,
+    email: {
+        type: String,
+        index: true,
+        match: /.+\@.+\..+/
+    },
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        validate: [
+            function(password) {
+                return password.length >= 6;
+            },
+            'Password should be longer'
+        ]
+    },
     created: {
-    	type: Date,
-    	default: Date.now
+        type: Date,
+        default: Date.now
+    },
+    role: {
+        type: String,
+        enum: ['Admin', 'Owner', 'User']
     }
+    website: {
+        type: String,
+        set: function(url) {
+            if (!url) {
+                return url;
+            } else {
+                if (url.indexOf('http://') !== 0 && url.indexOf('https://') !== 0) {}
+                return url;
+            }
+        }
+    },
 });
 
 mongoose.model('User', UserSchema);
-
